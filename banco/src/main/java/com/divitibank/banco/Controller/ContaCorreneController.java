f7063d6d8a2f94e54ed88923aeedbd5f9f9ce061
carvalho-joao <joao.carvalho@germinare.org.br>
2025-03-10 12:15:29 -0300

    tentando corrigir

--- banco/src/main/java/com/divitibank/banco/Controller/ContaCorreneController.java
+++ banco/src/main/java/com/divitibank/banco/Controller/ContaCorreneController.java
@@ -1,19 +1,27 @@
 package com.divitibank.banco.Controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.divitibank.banco.Entity.Cartao;
 import com.divitibank.banco.Entity.ContaCorrente;
 import com.divitibank.banco.Entity.Extrato;
 import com.divitibank.banco.Service.ContaCorrenteService;
 
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/contas")
 public class ContaCorreneController {
-    private ContaCorrenteService contaCorrenteService;
+    private final ContaCorrenteService contaCorrenteService;
 
     public ContaCorreneController(ContaCorrenteService contaCorrenteService) {
         this.contaCorrenteService = contaCorrenteService;
@@ -23,26 +31,29 @@ public class ContaCorreneController {
     public ContaCorrente salvarConta(@RequestBody ContaCorrente contaCorrente) {
         return contaCorrenteService.saveConta(contaCorrente);
     }
+    
     @GetMapping("/{cpf}/cartoes")
     public Optional<List<Cartao>> buscarCartaoUsuario(@PathVariable String cpf) {
         return contaCorrenteService.buscarCartao(cpf);
     }
+
     @GetMapping("/{cpf}/extrato")
     public Optional<List<Extrato>> buscarExtratoUsuario(@PathVariable String cpf) {
         return contaCorrenteService.buscarExtrato(cpf);
     }
+
     @GetMapping("/{cpf}/informacoes")
     public ContaCorrente buscarPorCpf(@PathVariable String cpf) {
         return contaCorrenteService.buscarInformacoesPorCpf(cpf);
     }
 
-    @DeleteMapping("/{cpf}")
+    @DeleteMapping("/{cpf}/excluir")
     public void deletarContaPorId(@PathVariable String cpf) {
         contaCorrenteService.excluirContaPorId(cpf);
     }
 
     @RequestMapping("/error")
-    public String handleError() {
-        return "error";
+    public ResponseEntity<String> handleError() {
+        return new ResponseEntity<>("Ocorreu um erro!", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
